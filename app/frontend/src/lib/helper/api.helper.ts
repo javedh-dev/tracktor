ee750e56d21699524a2cde454fa8fe04b199ff5e
Javed Hussain <javedh.dev@gmail.com>
2025-10-28 17:13:01 +0530

    Fixed API client baseURL to include /api path

--- app/frontend/src/lib/helper/api.helper.ts
+++ app/frontend/src/lib/helper/api.helper.ts
@@ -2,8 +2,9 @@ import { browser } from '$app/environment';
 import { env } from '$env/dynamic/public';
 import { HttpClient } from './http.helper';
 
+const baseURL = env.PUBLIC_API_BASE_URL || '';
 export const apiClient = new HttpClient({
-	baseURL: env.PUBLIC_API_BASE_URL || '/api',
+	baseURL: `${baseURL}/api`,
 	headers: {
 		'x-user-pin': browser ? localStorage.getItem('userPin') || '' : ''
 	},
