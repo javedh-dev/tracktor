f5edc0117b8c3cf2a71d05b28b6361348b7c8fd6
Javed Hussain <javed15895@gmail.com>
2025-12-14 22:12:47 +0530

    Added alerts for expiry of PUCC and insurance

--- src/lib/components/feature/overview/OverviewTab.svelte
+++ src/lib/components/feature/overview/OverviewTab.svelte
@@ -2,13 +2,20 @@
 	import TabContainer from '$appui/TabContainer.svelte';
 	import CostChart from './CostChart.svelte';
 	import MileageChart from './MileageChart.svelte';
+	import AlertsSection from './AlertsSection.svelte';
 </script>
 
 <TabContainer title="Overview">
 	<div class="overflow-x-auto">
-		<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-			<MileageChart />
-			<CostChart />
+		<div class="space-y-6">
+			<!-- Alerts Section -->
+			<AlertsSection />
+
+			<!-- Charts Section -->
+			<div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+				<MileageChart />
+				<CostChart />
+			</div>
 		</div>
 	</div>
 </TabContainer>
